68b2c49914621aa6d9e3b194c7e042a5d02a13b7
Manel Matougui <m.matougui@gmail.com>
2020-01-26 20:38:29 -0800

    add credits

---
+++ ConnectMe/Buttons/CreditsButton.swift
@@ -0,0 +1,29 @@
+//
+//  CreditsButton.swift
+//  ConnectMe
+//
+//  Created by Manel matougui on 1/26/20.
+//  Copyright © 2020 Manel matougui. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+class CreditsButton : SKSpriteNode {
+    // initialize playButton
+    // we need constructor to enable user interaction
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        isUserInteractionEnabled = true
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>,with event: UIEvent?) {
+       if let game = SKScene(fileNamed: "CreditsScene") {
+           // Set the scale mode to scale to fit the window
+           game.scaleMode = .aspectFill
+           // Present the scene
+           scene?.view?.presentScene(game)
+       }
+    }
+    
+
+}
